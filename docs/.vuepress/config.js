835e1e1f7941d4b45958876fdfd30d14c291b617
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-23 21:45:34 +0300

    Initial commit with sources.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,67 @@
+module.exports = {
+    base: '/',
+    locales: {
+        '/': {
+            lang: 'en-US',
+            logo: '/logo.svg',
+            title: 'Project Essentials',
+            description: 'Make forge servers great again. 🎉'
+        },
+        '/ru/': {
+            lang: 'ru-RU',
+            logo: '/logo.svg',
+            title: 'Project Essentials',
+            description: 'Make forge servers great again. 🎉'
+        }
+    },
+    themeConfig: {
+        logo: '/logo.svg',
+        locales: {
+            '/': {
+                lastUpdated: 'Last Updated',
+                selectText: 'Languages',
+                label: 'English',
+                ariaLabel: 'Languages',
+                editLinkText: 'Edit this page on GitHub',
+                serviceWorker: {
+                    updatePopup: {
+                        message: "New content is available.",
+                        buttonText: "Refresh"
+                    }
+                },
+                algolia: {},
+                nav: [
+                    { text: 'Home', link: '/' },
+                    { text: 'Download', link: '/download/' },
+                    { text: 'User Guide', link: '/guide/' },
+                    { text: 'API', link: '/api/' },
+                    { text: 'About', link: '/about/' },
+                    { text: 'GitHub', link: 'https://github.com/ProjectEssentials' }
+                ],
+                sidebar: 'auto'
+            },
+            '/ru/': {
+                lastUpdated: 'Последнее обновление',
+                selectText: 'Языки',
+                label: 'Русский',
+                editLinkText: 'Отредактируйте эту страницу на GitHub',
+                serviceWorker: {
+                    updatePopup: {
+                        message: "Контент на этой странице был изменен.",
+                        buttonText: "Обновить"
+                    }
+                },
+                nav: [
+                    { text: 'Главная', link: '/' },
+                    { text: 'Скачать', link: '/download/' },
+                    { text: 'Руководство пользователя', link: '/guide/' },
+                    { text: 'API', link: '/api/' },
+                    { text: 'О проекте', link: '/about/' },
+                    { text: 'GitHub', link: 'https://github.com/ProjectEssentials' }
+                ],
+                algolia: {},
+                sidebar: 'auto'
+            }
+        }
+    }
+}
---
+++ docs/readme.md
@@ -0,0 +1,21 @@
+---
+home: true
+heroImage: /logo.svg
+heroText: Project Essentials
+tagline: Make your forge server 🎉
+actionText: Get Started ↠
+actionLink: /guide/
+features:
+- title: Easy to use
+  details: Just drag and drop the module files into the mods folder, and enjoy the game.
+- title: Module system
+  details: Install only the modules you need. It will also help reduce server overload.
+- title: Client independence
+  details: All modules are independent of their presence on the client. The project uses safe localization for such cases.
+- title: Really essentials
+  details: We have a many modules, 8 modules + 1 core module + 2 future modules + 1 module in review.
+- title: Performance
+  details: Project modules do not overload the server even if all (9) modules are installed.
+- title: Multiversion support
+  details: Currently I support two versions of minecraft `1.14.4` and `1.15.2` with Forge. And I'll support more versions.
+---
