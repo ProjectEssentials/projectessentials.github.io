4c4449df1bb4e73d18dc4c492c406fe39e3f578e
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-25 16:35:49 +0300

    deploy.sh file cleanup.

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env sh
 
-# abort on errors
+set -e
 
-# build
 npm run build
-
 cp -r ./docs/.vuepress/dist/. ./
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
 git push
 
-cd -
+cd - || exit
