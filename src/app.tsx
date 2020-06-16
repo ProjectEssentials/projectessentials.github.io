2884f1359d8004fde9ea94f20389d5d578ec1f8e
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-16 21:39:12 +0300

    app.tsx navigation support added.

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,23 @@
-import React, {Component} from 'react';
-import Header from '@jetbrains/ring-ui/components/header/header';
-import Footer from '@jetbrains/ring-ui/components/footer/footer';
-import './app.css';
+import React, {Component} from 'react'
+import Footer from '@jetbrains/ring-ui/components/footer/footer'
+import {pushStateLocationPlugin, UIRouter, UIView} from "@uirouter/react"
+import Top from './components/top/top'
+import Home from "./components/home/home"
+import './app.css'
+
+const states = [
+  {name: 'base', url: '/', component: Home}
+]
+
+const plugins = [pushStateLocationPlugin]
 
 export default class App extends Component {
   render = () => (
     <div>
-      <Header>
-        <a href="/">Home</a>
-      </Header>
-      <div className="app-content">
-        {'App content'}
-      </div>
-      <Footer/>
+      <Top/>
+      <UIRouter plugins={plugins} states={states}>
+        <UIView/>
+      </UIRouter>
     </div>
   )
 }
