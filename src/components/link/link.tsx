04cdc38aae497524ef52f095a0bd0db966241c77
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-17 21:36:04 +0300

    Link component implemented.

---
+++ src/components/link/link.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './link.css'
+
+interface LinkProperties {
+  href: string
+  content: string
+  className: string
+}
+
+export default class Link extends React.Component<LinkProperties, any> {
+  render = () => (
+    <a className={`link ${this.props.className}`} href={this.props.href}>{this.props.content.trim()}</a>)
+}
