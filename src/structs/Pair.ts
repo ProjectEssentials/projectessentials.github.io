11e9bd401f3ea781676cd18a71759d329000f30d
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-17 21:35:21 +0300

    `Pair.ts` struct implemented.

---
+++ src/structs/Pair.ts
@@ -0,0 +1,51 @@
+export namespace Struct {
+  export interface Pair<K, V> {
+    first: K
+    second: V
+  }
+
+  export class ImmutablePair<K, V> implements Pair<K, V> {
+    private readonly _first: K
+    private readonly _second: V
+
+    public constructor(first: K, second: V) {
+      this._first = first
+      this._second = second
+    }
+
+    public get second() {
+      return this._second
+    }
+
+    public get first() {
+      return this._first
+    }
+  }
+
+  export class MutablePair<K, V> implements Pair<K, V> {
+    private _first: K
+    private _second: V
+
+    public constructor(first: K, second: V) {
+      this._first = first
+      this._second = second
+    }
+
+    get first() {
+      return this._first
+    }
+
+    set first(value: K) {
+      this._first = value
+    }
+
+    get second() {
+      return this._second
+    }
+
+    set second(value: V) {
+      this._second = value
+    }
+  }
+}
+
