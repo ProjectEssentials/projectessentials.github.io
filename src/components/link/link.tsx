d9b8dce8f9ce12a569d54415d5eda2bdc3dad5c2
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-17 21:41:14 +0300

    link.tsx formatted.

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -10,5 +10,11 @@ interface LinkProperties {
 
 export default class Link extends React.Component<LinkProperties, any> {
   render = () => (
-    <a className={`link ${this.props.className}`} href={this.props.href}>{this.props.content.trim()}</a>)
+    <a
+      className={`link ${this.props.className}`}
+      href={this.props.href}
+    >
+      {this.props.content.trim()}
+    </a>
+  )
 }
