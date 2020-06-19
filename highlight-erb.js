4226d534d646bf0e53c33944b04a9d45892e092c
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-19 23:41:08 +0300

    Updates

--- highlight-erb.js
+++ highlight-erb.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{460:function(n,e){n.exports=function(n){return{subLanguage:"xml",contains:[n.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{461:function(n,e){n.exports=function(n){return{subLanguage:"xml",contains:[n.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
 //# sourceMappingURL=highlight-erb.js.map
