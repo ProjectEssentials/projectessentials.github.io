a6e44eea567edff6c245875078ec18063f9a6ea9
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-19 22:54:26 +0300

    Updates

--- highlight-clojure-repl.js
+++ highlight-clojure-repl.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{440:function(n,s){n.exports=function(n){return{contains:[{className:"meta",begin:/^([\w.-]+|\s*#_)?=>/,starts:{end:/$/,subLanguage:"clojure"}}]}}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{442:function(n,s){n.exports=function(n){return{contains:[{className:"meta",begin:/^([\w.-]+|\s*#_)?=>/,starts:{end:/$/,subLanguage:"clojure"}}]}}}}]);
 //# sourceMappingURL=highlight-clojure-repl.js.map
--- highlight-mojolicious.js
+++ highlight-mojolicious.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[78],{498:function(e,n){e.exports=function(e){return{subLanguage:"xml",contains:[{className:"meta",begin:"^__(END|DATA)__$"},{begin:"^\\s*%{1,2}={0,2}",end:"$",subLanguage:"perl"},{begin:"<%{1,2}={0,2}",end:"={0,1}%>",subLanguage:"perl",excludeBegin:!0,excludeEnd:!0}]}}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[78],{500:function(e,n){e.exports=function(e){return{subLanguage:"xml",contains:[{className:"meta",begin:"^__(END|DATA)__$"},{begin:"^\\s*%{1,2}={0,2}",end:"$",subLanguage:"perl"},{begin:"<%{1,2}={0,2}",end:"={0,1}%>",subLanguage:"perl",excludeBegin:!0,excludeEnd:!0}]}}}}]);
 //# sourceMappingURL=highlight-mojolicious.js.map
