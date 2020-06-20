a017a13363426a45fe71b3575e3735a01a17473d
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-20 10:42:55 +0300

    publish-process.sh updated.

--- publish-process.sh
+++ publish-process.sh
@@ -2,4 +2,4 @@
 cp ./dist/index.html ./dist/200.html
 cp ./dist/index.html ./dist/404.html
 cd dist || exit
-find . -maxdepth 1 ! -name '200.html' ! -name '404.html' ! -name 'favicon.ico' ! -name 'index.html' ! -name 'main.js' ! -name 'highlight-properties.js' ! -name 'highlight-plaintext.js' -type f -exec rm -v {} +
+find . -maxdepth 1 ! -name '200.html' ! -name '404.html' ! -name 'favicon.ico' ! -name 'index.html' ! -name 'main.js' ! -name 'highlight-properties.js' ! -name 'highlight-plaintext.js' ! -name 'highlight-json.js' -type f -exec rm -v {} +
