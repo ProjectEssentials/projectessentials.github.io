521c704a8813aa2bf69d13147ebeb982b07f1bda
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-20 10:40:38 +0300

    app.tsx updated.

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,9 @@
 import React, {Component} from 'react'
-import {pushStateLocationPlugin, UIRouter, UIView} from "@uirouter/react"
+import {UIRouter, UIView} from "@uirouter/react"
 import Top from './components/top/top'
-import Home from "./components/home/home"
-import Download from "./components/download/download"
+
 import './app.css'
-import Manual from "./components/manual/manual";
-
-const states = [
-  {name: 'base', url: '/', component: Home},
-  {name: 'download', url: '/download', component: Download},
-  {name: 'manual', url: '/manual', component: Manual}
-]
-
-const plugins = [pushStateLocationPlugin]
+import {plugins, states} from "./index";
 
 export default class App extends Component {
   render = () => (
